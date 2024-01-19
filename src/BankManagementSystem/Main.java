1c710f64c30ad76ccc01ed60b01cf6cf1a6d08c9
niroj satyal <niroj.satyal2000@gmail.com>
2024-01-19 17:44:59 +0545

    add env.properties files

--- src/BankManagementSystem/Main.java
+++ src/BankManagementSystem/Main.java
@@ -4,12 +4,36 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Scanner;
+import java.io.*;
+import java.util.Properties;
 
 public class Main {
-    private static String url = "jdbc:mysql://localhost:3306/banking_system";
-    private static String db_name ="root";
-    private static  String db_password="jorin!@#1";
+    private static String db_url ;
+    private static String db_username;
+    private static  String db_password;
+
+
+    static {
+        loadDatabaseProperties();
+    }
+
+    private static void loadDatabaseProperties() {
+        try (InputStream input = Main.class.getClassLoader().getResourceAsStream("BankManagementSystem/env.properties")) {
+            Properties properties = new Properties();
+            if (input != null) {
+                properties.load(input);
+                db_url = properties.getProperty("db_url");
+                db_username = properties.getProperty("db_username");
+                db_password = properties.getProperty("db_password");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) throws SQLException ,ClassNotFoundException {
+        System.out.println(db_password);
+        System.out.println(db_username);
+        System.out.println(db_url);
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver") ;
@@ -23,7 +47,7 @@ public class Main {
 
         try
         {
-            Connection conn= DriverManager.getConnection(url,db_name,db_password);
+            Connection conn= DriverManager.getConnection(db_url,db_username,db_password);
             Scanner scanner=new Scanner(System.in);
             System.out.println("connection Established successfully....");
 
@@ -32,6 +56,7 @@ public class Main {
         catch(SQLException e)
         {
             System.out.println(e.getMessage());
+
         }
 
 
